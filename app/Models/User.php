f802b8ac6f39c4473b8b4500a281f70495a76768
Sandi Indika Saputra <sandidikaputra@gmail.com>
2024-07-31 16:02:34 +0700

    feat: Edit users table structure (id -> uuid) & login user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Traits\UuidTrait;
+
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, UuidTrait;
 
     /**
      * The attributes that are mass assignable.
